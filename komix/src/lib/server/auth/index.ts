17b1013b3ffeac4abd9d6078d49c8305f546e7ce
LeCimmerien <bgp.connan@gmail.com>
2026-04-17 10:12:51 +0200

    switch to argon2id

--- komix/src/lib/server/auth/index.ts
+++ komix/src/lib/server/auth/index.ts
@@ -1,13 +1,12 @@
-import bcrypt from 'bcrypt';
+import argon2 from 'argon2';
 
 export type HashedPassword = string & { readonly __brand: unique symbol };
 
 export async function hashPassword(plain: string): Promise<HashedPassword> {
-    const saltRounds = 12;
-    const hash = await bcrypt.hash(plain, saltRounds);
+    const hash = await argon2.hash(plain, { type: argon2.argon2id });
     return hash as HashedPassword;
 }
 
 export async function checkHash(plain: string, hashedPassword: HashedPassword): Promise<boolean> {
-    return bcrypt.compare(plain, hashedPassword);
+    return argon2.verify(hashedPassword, plain);
 }
